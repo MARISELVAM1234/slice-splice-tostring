def0e891996e469c96bfacb1981e3d3f4b610391
selvam <selvamplkm111@gmail.com>
2022-06-21 21:27:01 +0530

    add-folder.

---
+++ script.js
@@ -0,0 +1,22 @@
+//slice 
+//  var a = ["One","two","three","four","five","six","seven"]
+//  var b = a.slice(1,4)
+//  //var b = a.slice(-6,-2) // -6 starting value -2 end value
+//  console.log(b)
+// //  console.log(a);
+
+
+
+// //toString() to convert all the element in string
+ var a = ['hai','hello','welcome',1,2];
+var b = a.toString();
+console.log(typeof b)
+console.log(b) 
+
+
+
+//splice() to add or removing the element
+// let months = ["Jan", "Feb", "Mar", "April", "Aug", "Sep"]
+// toAdd = months.splice(4, 0, "May", "Jun", "July")
+// // toAdd = months.splice(3, 2)  //expected output ['Jan', 'Feb', 'Mar', 'Sep'] 3rd length la irukka kudiya 2 element ah remove pannum
+// console.log("To add the &delete the element :", months)
